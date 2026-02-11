d8a68289ebb024e7cbae454557eece2019d09d29
Khushi Kumari <217077736+SinghKhushi190@users.noreply.github.com>
2026-02-11 23:22:46 +0530

    Implement getMilk and bottle calculation functions
    Added functions to get milk and calculate bottles and change.

--- pratice.js
+++ pratice.js
@@ -1,8 +1,41 @@
-let a = 5;
-let b = 6;
-console.log("The value of a is :",a);
-console.log("The value of b is :",b);
-console.log("The sum of a nad b is:",a+b);
-console.log("The substraction of a nad b is:",a-b);
-console.log("The multi of a nad b is:",a*b);
-console.log("The Div of a nad b is:",a/b);
+function getMilk(money , costPerBottle)
+{
+  console.log("leaveHouse");
+  console.log("moveRight"); 
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+
+  console.log("buy" + calcBottle(money , costPerBottle ) + "bottles of milk");
+
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHome");
+
+  // return money % 1.5;  
+  return calcChange(money , 1.5);
+}
+
+function calcBottles(startingMoney , costPerBottle)
+{
+  var numberOfBottles = math.floor(startingMoney / costPerBottle);
+  return numberOfBottle;
+
+
+  function calcChange(startingAmount , costPerBottle)
+  {
+    var change  = startingAmount % costPerBottle;
+    return change;
+  }
+  console.log("Hello master , here is your " + getMilk() + " change");
+  
