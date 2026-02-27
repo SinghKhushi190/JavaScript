7ea195ac2a7928d00e89930d2945730589896536
Khushi Kumari <217077736+SinghKhushi190@users.noreply.github.com>
2026-02-27 09:26:45 +0530

    Implement list item creation in app.js
    Add functionality to create and append a list item.

---
+++ DOM/app.js
@@ -0,0 +1,18 @@
+// title.style.color = 'red';
+
+// const listItems = document.querySelectorAll('.list-items');
+
+// for(i = 0; i < listItems.length; i++){
+//     listItems[i].style.frontSize = '2rem';
+// }
+
+
+// creating elements
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+ul.append(li);
+
+
+console.log();
