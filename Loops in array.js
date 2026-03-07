8b191fc6a6c71a194385e5203b499849017df0ae
Khushi Kumari <217077736+SinghKhushi190@users.noreply.github.com>
2026-03-07 13:24:46 +0530

    Refactor array and object loop examples

--- Loops in array.js
+++ Loops in array.js
@@ -6,22 +6,23 @@ for (let index = 0; index < a.length; index++) {
 }
 
 
-// a.forEach((value, index ,arr)=>{
-//     console.log(value, index, arr);
-// })
+a.forEach((value, index ,arr)=>{
+    console.log(value, index, arr);
+})
 
-// let obj = {
-//     a: 1,
-//     b: 2,
-//     c: 3,
-// }
-// for (const key in obj) {
-//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
-//         const element = obj[key];
-//     console.log(key, element)
-//     }
-// }
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+}
+for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+    console.log(key, element)
+    }
+}
 
 for (const iterator of a) {
     console.log(iterator);
 }
+
