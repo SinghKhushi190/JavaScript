2ce1101c546659db4f26a65db9411dd30f2fce66
Khushi Kumari <217077736+SinghKhushi190@users.noreply.github.com>
2026-03-09 22:20:39 +0530

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -161,5 +161,18 @@ SuperHero.prototype = Object.create(person.prototype)
 
 const batman = new SuperHero('Bruce' , 'Wayne')
 SuperHero.prototype.constructor = SuperHero
-console.log(batman.getFullName())
+console.log(batman.getFullName())  // Bruce Wayne
+// Classes
+class Person{
+    constructor(fName , lName){
+        this.firstName = fName
+        this.lastName  = lName
+    }
 
+    sayMyName(){
+        return this.firstName + ' ' + this.lastName
+    }
+}
+
+const classP1 = new Person('Bruce' , 'Wayne')
+console.log(classP1.sayMyName()) // Bruce Wayne
