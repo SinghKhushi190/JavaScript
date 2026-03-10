d7c3301bfd673933148e57b09381071b17427ac0
Khushi Kumari <217077736+SinghKhushi190@users.noreply.github.com>
2026-03-10 22:11:06 +0530

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -188,3 +188,30 @@ class SuperHero extends Person{
 }
 const batman = new SuperHero('Spider' , 'Man')
 console.log(batman.sayMyName())
+// Generator Function
+const obj = {
+    [Symbol.iterator] : function(){
+        let step = 0
+        const iterator = {
+            next : function (){
+                step++
+                if(step === 1){
+                    return {value : 'Hello' , done : 'false'}
+                }else if(step ===2){
+                    return{value : 'World' ,  done : 'false'}
+                }
+                return {value : undefined , done : true}
+            },
+        }
+        return iterator
+    },
+}
+function* generatorFunction(){
+    yield 'Hello'
+    yield 'world'
+}
+
+const genertatorObject = generatorFunction()
+for(const word of genertatorObject) {
+    console.log(word)   // Hello
+}                          // World
