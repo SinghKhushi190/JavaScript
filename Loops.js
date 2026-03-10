cfa49bb98ffc6e26a3187426896ed4133bfacb30
Khushi Kumari <217077736+SinghKhushi190@users.noreply.github.com>
2026-03-10 21:55:38 +0530

    Update Loops.js

--- Loops.js
+++ Loops.js
@@ -38,3 +38,24 @@ for(const num of numArray){
     console.log('Iterartion' + num)
 }
 
+// iterable and iteration 
+const obj = {
+    [Symbol.iterator] : function(){
+        let step = 0
+        const iterator = {
+            next : function (){
+                step++
+                if(step === 1){
+                    return {value : 'Hello' , done : 'false'}
+                }else if(step ===2){
+                    return{value : 'World' ,  done : 'false'}
+                }
+                return {value : undefined , done : true}
+            },
+        }
+        return iterator
+    },
+}
+ for(const word of obj){
+            console.log(word)
+ }
