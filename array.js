3f901392fe8d672b28a09c319f11822080ad5aa0
Khushi Kumari <217077736+SinghKhushi190@users.noreply.github.com>
2026-03-14 21:51:37 +0530

    Refactor array.js with additional operations and examples

--- array.js
+++ array.js
@@ -34,4 +34,64 @@ numbers.splice(1,44,444)
 
 console.log(numbers) // Splice modifies the array in place
 
+let marks = [97,78,67,99,67,89];
 
+console.log(marks);
+console.log(typeof marks);
+console.log(marks.length);        // length property
+
+
+console.log(marks[0]);          // accessing elements
+
+let heros = ["ironman","spiderman","thor","hulk"];
+console.log(heros);
+console.log(heros.length);
+
+marks[0] = 100;// updating elements                
+console.log(marks);
+
+for(let idex=0; idex < heros.length; idex++){
+    console.log(heros[idex]);            // through for loop
+}
+
+// for of loop
+
+for(let hero of heros){
+    console.log(hero);
+}
+
+let cities = ["pune","mumbai","banglore","chennai"];
+
+for(let city of cities){
+    console.log(city.toUpperCase());
+}
+
+let marks = [85,97, 44, 37, 76, 60];
+
+let sum = 0;
+
+for(let val of marks){
+     sum += val;
+}
+console.log(`sum of marks = ${sum}`);
+let avg = sum / marks.length;
+console.log(`avg marks of the class = ${avg}`);
+
+let items = [250,645,300,900,50];
+
+let i = 0;
+for(let val of items){
+    // console.log(`value of index ${i} = ${val}`);
+    let offer = val / 10;
+    items[i] = items[i] - offer;
+    console.log(`value after offer = ${items[i]}`);
+    i++;
+}
+
+ for(let i =0 ; i < items.length; i++){
+    let offer = items[i] / 10;
+    items[i] -= offer;
+    // console.log(`value after offer = ${items[i]}`);
+ }
+
+ console.log(items);
